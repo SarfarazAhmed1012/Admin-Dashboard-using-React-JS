dfc47e691459aca90dc9e1287aed78d95bd03f5c
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2022-09-10 01:06:35 +0500

    navbar added

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { IoIosMore } from "react-icons/io";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 
+import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
+import {
+  earningData,
+  medicalproBranding,
+  recentTransactions,
+  weeklyStats,
+  dropdownData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../context/ContextProvider";
+import product9 from "../data/product9.jpg";
 const Ecommerce = () => {
   return (
-    <div>Ecommerce</div>
-  )
-}
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center"></div>
+    </div>
+  );
+};
 
-export default Ecommerce
+export default Ecommerce;

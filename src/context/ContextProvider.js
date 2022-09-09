a4c5393283e01730bbd2bc83d6325293b56668e7
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2022-09-09 16:21:51 +0500

    added sidebar

---
+++ src/context/ContextProvider.js
@@ -0,0 +1,21 @@
+import React, { Children, createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  notification: false,
+  cart: false,
+  userProfile: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  return (
+    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
